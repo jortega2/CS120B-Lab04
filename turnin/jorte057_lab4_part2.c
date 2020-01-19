3851af4b06cb46f4287492290d22e4c773a7fddb
Juvenal Ortega <jorte057@wch136-15.cs.ucr.edu>
2020-01-19 13:49:34 -0800

    Completed part 2

---
+++ turnin/jorte057_lab4_part2.c
@@ -0,0 +1,106 @@
+/*	Author: jorte057
+ *  Partner(s) Name: Duke Pham
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+//
+//
+// init- start of sm
+// on1 - turn on PB0, turn off PB1, stay in the state if user still has PBA held down to prevent unwanted changes
+// wait1 - no changes to pins, stay in state until user presses PA0 to switch lights
+// on2 - turn off PB0, turn on PB1, stay in the state if user still has PBA held down to prevent unwanted changes
+// wait2 - no changes to pins, stay in state until user presses PA0 to switch lights
+enum add_states { init, interphase, inc, incWait, dec, decWait, reset } addsm;
+
+void add_states(){
+	//transitions
+	switch(addsm) {
+		case init:
+			addsm = interphase;
+			break;
+		case interphase:
+			if (PINA == 0x01){
+				addsm = inc;
+			}
+			else if (PINA == 0x02){
+				addsm = dec;
+			}else if (PINA == 0x00){
+				addsm = reset;
+			} else {
+				addsm = interphase;
+			}
+			break;
+		case inc:
+			addsm = incWait;
+			break;
+		case incWait: 
+			if (PINA == 0x01){
+				addsm = incWait;
+			}else{
+				addsm = interphase;
+			}
+			break;
+		case dec:
+			addsm = decWait;
+			break;
+		case decWait:
+                        if (PINA == 0x02){
+                                addsm = decWait;
+                        }else{
+                                addsm = interphase;
+                        }
+			break;
+		case reset:
+			addsm = interphase;
+			break;
+		default:
+			addsm = interphase;
+			break;
+	}//transitions
+	
+	//state actions
+	switch(addsm) {
+                case init:
+			PORTC = 0x07;
+                        break;
+                case interphase:
+			break;
+                case inc:
+			if (PORTC < 0x09)
+				PORTC = (PORTC + 0x01);
+                        break;
+		case incWait:
+			break;
+                case dec:
+			if (PORTC > 0x00)
+                        	PORTC = (PORTC - 0x01);
+                        break;
+		case decWait:
+			break;
+                case reset:
+			PORTC = 0x07;
+                        break;
+                default:
+                        break;
+        }//state actins
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	PORTC = 0x07;
+	addsm  = init;
+    /* Insert your solution below */
+    while (1) {
+	add_states();
+    }
+    return 1;
+}
