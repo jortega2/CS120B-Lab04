68a12f971653518ceb93ed85650ac4c1e61d3273
Juvenal Ortega <jorte057@wch136-15.cs.ucr.edu>
2020-01-18 20:00:17 -0800

    Completed part 1

---
+++ test/tests.gdb
@@ -0,0 +1,60 @@
+# Test file for "Lab4_stateMachines"
+
+
+# commands.gdb provides the following functions for ease:
+#   test "<message>"
+#       Where <message> is the message to print. Must call this at the beginning of every test
+#       Example: test "PINA: 0x00 => expect PORTC: 0x01"
+#   checkResult
+#       Verify if the test passed or failed. Prints "passed." or "failed." accordingly, 
+#       Must call this at the end of every test.
+#   expectPORTx <val>
+#       With x as the port (A,B,C,D)
+#       The value the port is epected to have. If not it will print the erroneous actual value
+#   setPINx <val>
+#       With x as the port or pin (A,B,C,D)
+#       The value to set the pin to (can be decimal or hexidecimal
+#       Example: setPINA 0x01
+#   printPORTx f OR printPINx f 
+#       With x as the port or pin (A,B,C,D)
+#       With f as a format option which can be: [d] decimal, [x] hexadecmial (default), [t] binary 
+#       Example: printPORTC d
+#   printDDRx
+#       With x as the DDR (A,B,C,D)
+#       Example: printDDRB
+
+echo ======================================================\n
+echo Running all tests..."\n\n
+#test sequeence from init: !A0, A0, A0, A0, => PORTB: 1
+test "PINA: 0x00, 0x01, 0x01, 0x01 => PB0 = 0, PB1 = 1, state:on2"
+set sm = init 
+setPINA 0x00
+continue 2
+setPINA 0x01
+continue 2
+setPINA 0x01
+continue 2
+setPINA 0x01
+continue 2
+expectPORTB 0x02
+expect sm on2
+checkResult
+
+#test sequeence from on2: !A0, A0,! A0, => PORTB: 1
+test "PINA: 0x00, 0x01, 0x00  => PB0 = 1, PB1 = 0, state:wait1"
+set sm = on2
+setPINA 0x00
+continue 2
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+expectPORTB 0x01
+expect sm wait1
+checkResult
+# Add tests below
+
+# Report on how many tests passed/tests ran
+set $passed=$tests-$failed
+eval "shell echo Passed %d/%d tests.\n",$passed,$tests
+echo ======================================================\n
---
+++ turnin/jorte057_lab4_part1.c
@@ -0,0 +1,95 @@
+/*	Author: jorte057
+ *  Partner(s) Name: Duke Pham
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+//
+//
+// init- start of sm
+// on1 - turn on PB0, turn off PB1, stay in the state if user still has PBA held down to prevent unwanted changes
+// wait1 - no changes to pins, stay in state until user presses PA0 to switch lights
+// on2 - turn off PB0, turn on PB1, stay in the state if user still has PBA held down to prevent unwanted changes
+// wait2 - no changes to pins, stay in state until user presses PA0 to switch lights
+enum sm_states { init, on1, wait1, on2, wait2 } sm;
+
+void sm_tick(){
+	//transitions
+	switch(sm) {
+		case init:
+			sm = on1;
+			break;
+		case on1:
+			if (PINA == 0x01){
+				sm = on1;
+			}
+			else if (PINA == 0x00){
+				sm = wait1;
+			}
+			break;
+		case wait1:;
+                        if (PINA == 0x01){
+                                sm = on2;
+			}
+                        else if (PINA == 0x00){
+                                sm = wait1;
+			}
+			break;
+		case on2:
+                        if (PINA == 0x01){
+                                sm = on2;
+			}
+                        else if (PINA == 0x00){
+                                sm = wait2;
+			}
+			break;
+		case wait2:
+                        if (PINA == 0x01){
+                                sm = on1;;
+			}
+                        else if (PINA == 0x00){
+                                sm = wait2;
+			}
+			break;
+		default:
+			sm = init;
+			break;
+	}//transitions
+	
+	//state actions
+	switch(sm) {
+                case init:
+                        break;
+                case on1:
+                        PORTB = 0x01;
+			break;
+                case wait1:
+                        break;
+                case on2:
+                        PORTB = 0x02;
+                        break;
+                case wait2:
+                        break;
+                default:
+                        break;
+        }//state actins
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	PORTB = 0x01;
+	sm  = init;
+    /* Insert your solution below */
+    while (1) {
+	sm_tick();
+    }
+    return 1;
+}
